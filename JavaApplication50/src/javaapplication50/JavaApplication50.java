ed9bb71fcdb25c69377887d2d0bf786ff800e4d3
Jonatan Ballestero <jonatan.a.ballestero@gmail.com>
2023-08-08 11:23:08 -0300

    Cambios para merge

--- JavaApplication50/src/javaapplication50/JavaApplication50.java
+++ JavaApplication50/src/javaapplication50/JavaApplication50.java
@@ -6,7 +6,7 @@ public class JavaApplication50 {
    
     public static void main(String[] args) {
 
-        System.out.println("Jon");
+        System.out.println("otrohfgjhkjk");
 
         System.out.println("Probando merges diegoginard");
         
